9625d468634b054bc2def040718d86ad6cc259af
alexander <alexxk5111@gmail.com>
2025-01-26 19:08:11 -0800

    added github pages

--- lolrus/next.config.ts
+++ lolrus/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/lolruscoinproject/' : '',
+  basePath: isProd ? '/lolruscoinproject' : '',
+  output: 'export'
 };
 
 export default nextConfig;
